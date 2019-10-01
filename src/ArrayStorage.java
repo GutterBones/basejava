88d6045514189268e8316d4f764d1f882ff5ccfb
GutterBones <alexanderurfu@gmail.com>
2019-10-01 19:00:03 +0300

    ArrayStorage complete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,51 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    // storage elements counter
+    private int counter;
 
     void clear() {
+        for (int i = 0; i < counter; i++)
+            storage[i] = null;
+        counter = 0;
     }
 
     void save(Resume r) {
+        storage[counter] = r;
+        counter++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < counter; i++) {
+            if (storage[i].uuid.equals(uuid))
+            return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < counter; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                //for (int j = i; j < counter; j++)
+                // storage[j] = storage[j+1];
+                System.arraycopy(storage, i + 1, storage, i, counter - i);
+                counter--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] buff = new Resume[counter];
+        //for (int i = 0; i < counter; i++)
+        //      buff[i] = storage[i];
+        System.arraycopy(storage, 0, buff, 0, counter);
+        return buff;
     }
 
     int size() {
-        return 0;
+        return counter;
     }
 }
